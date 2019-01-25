e46fc7bc63d1ff952952248fe043207e6a10c9a5
bruno aguilar <josebrunoaguilar@gmail.com>
2019-01-24 17:47:53 -0700

    Inicio del proyecto sonidos

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,47 @@
 import { Component } from '@angular/core';
-
+import { ANIMALES } from '../../data/data.animales';
+import { Animal } from '../interfaces/animal.interface';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+    animales: Animal[] = [];
+    audio = new Audio();
+    audioTiempo: any;
+    constructor() {
+       this.animales = ANIMALES.slice(0);
+    }
+    reproducir(animal: Animal) {
 
+      this.pausar_audio(animal);
+
+      if( animal.reproduciendo ){
+        animal.reproduciendo = false;
+        return;
+      }
+
+      console.log(animal);
+      // audio = new Audio();
+      this.audio.src = animal.audio;
+      this.audio.load();
+      this.audio.play();
+
+      animal.reproduciendo = true;
+
+      this.audioTiempo = setTimeout(() => animal.reproduciendo = false, animal.duracion * 1000);
+    }
+
+    private pausar_audio(animalSel: Animal) {
+        clearTimeout( this.audioTiempo );
+        this.audio.pause();
+        this.audio.currentTime = 0;
+
+        for(let animal of this.animales){
+           if(animal.nombre != animalSel.nombre){
+             animal.reproduciendo = false;
+           }
+        }
+    }
 }
